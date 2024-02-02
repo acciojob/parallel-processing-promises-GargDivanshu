4415695429ead7da66663064d76f1fa935e5d3d8
GargDivanshu <96184674+GargDivanshu@users.noreply.github.com>
2024-02-02 17:20:39 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,4 +1,3 @@
-//your JS code here. If required.
 const output = document.getElementById("output");
 const btn = document.getElementById("download-images-button");
 
@@ -7,3 +6,31 @@ const images = [
   { url: "https://picsum.photos/id/238/200/300" },
   { url: "https://picsum.photos/id/239/200/300" },
 ];
+
+btn.addEventListener("click", () => downloadAndDisplayImages(images));
+
+async function downloadAndDisplayImages(images) {
+  // Create an array of Promises to download each image
+  const promises = images.map((image) => {
+    return new Promise((resolve, reject) => {
+      const img = new Image();
+      img.onload = () => resolve(img);
+      img.onerror = () =>
+        reject(new Error(`Failed to load image's URL: ${image.url}`));
+      img.src = image.url;
+    });
+  });
+
+  // Use Promise.all to download all images in parallel
+  return Promise.all(promises)
+    .then((images) => {
+      console.log(images.length);
+      // Once all images are downloaded, display them on the webpage
+      images.forEach((image) => {
+        output.appendChild(image);
+      });
+    })
+    .catch((err) => {
+      console.error(err);
+    });
+}
